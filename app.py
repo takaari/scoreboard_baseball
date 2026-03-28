9c19f0d8d9d9c88794101f00060249f2e95c89cb
takaari <kuniopop@gmail.com>
2026-03-28 23:24:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,23 @@ import streamlit as st
 import random
 import base64
 
+def play_sound_autoplay(file_path):
+    import base64
+    import uuid
 
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+
+    unique_id = uuid.uuid4()
+
+    audio_html = f"""
+    <audio autoplay id="{unique_id}" style="display:none;">
+        <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+    </audio>
+    """
+
+    st.markdown(audio_html, unsafe_allow_html=True)
 
 #def play_bgm_loop(file_path):
 #    with open(file_path, "rb") as f:
@@ -84,23 +100,7 @@ button:hover {
 </style>
 """, unsafe_allow_html=True)
 
-def play_sound_autoplay(file_path):
-    import base64
-    import uuid
 
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-
-    unique_id = uuid.uuid4()
-
-    audio_html = f"""
-    <audio autoplay id="{unique_id}" style="display:none;">
-        <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-    </audio>
-    """
-
-    st.markdown(audio_html, unsafe_allow_html=True)
 
 # -------------------------
 # セッション初期化
